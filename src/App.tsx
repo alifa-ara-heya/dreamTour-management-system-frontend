88766ee4abebda22069ed17bf2b5b0f0bc9804a5
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-08-08 16:53:47 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>DreamTour management System</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
